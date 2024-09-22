7dfcd9c2ae7c19536abc5e887578a2d3f672a45a
ashwani maurya <ashwanimaurya098@gmail.com>
2024-09-22 23:45:06 +0530

    app updated

--- App.js
+++ App.js
@@ -1,89 +1,16 @@
-
 import React, { useEffect, useRef, useState } from 'react';
-import { BackHandler, View, Alert, StyleSheet, Linking, StatusBar, SafeAreaView } from 'react-native';
+import { BackHandler, View, Alert, Text, StyleSheet } from 'react-native';
 import { WebView } from 'react-native-webview';
 import Spinner from 'react-native-loading-spinner-overlay';
 import NetInfo from '@react-native-community/netinfo';
-import ErrorPage from './ErrorPage.js';
-import LinearGradient from 'react-native-linear-gradient';
+import ErrorPage from './ErrorPage';
 
 const App = () => {
   const webViewRef = useRef(null);
   const [canGoBack, setCanGoBack] = useState(false);
   const [showSpinner, setShowSpinner] = useState(true);
   const [isConnected, setIsConnected] = useState(null);
-  const [CurrentUrl, setCurrentUrl] = useState('https://appskvt.charvns.com/');
-  const injectedJavaScript = `
-  const meta = document.createElement('meta');
-  meta.setAttribute('name', 'viewport');
-  meta.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no');
-  document.getElementsByTagName('head')[0].appendChild(meta);
-  true;
-`;
-  useEffect(() => {
-    if (CurrentUrl === 'https://shrikashiprasadam.com/') {
-      // Alert.alert('url is changed')
-      // Ensure the spinner is shown before navigating
-      setShowSpinner(true);
-      // Delay the URL change to ensure spinner is visible
-      // setTimeout(() => {
-      Linking.openURL('https://shrikashiprasadam.com/');
-      const redirectScript = "window.location.href = 'https://appskvt.charvns.com/';";
-      webViewRef.current.injectJavaScript(redirectScript);
-      // }, 100); // Adjust the delay if needed
-    }
 
-    if (CurrentUrl === 'https://www.instagram.com/shrikashivishwanath/') {
-      setShowSpinner(true);
-      Linking.openURL('https://www.instagram.com/shrikashivishwanath/');
-      const redirectScript = "window.location.href = 'https://appskvt.charvns.com/';";
-      webViewRef.current.injectJavaScript(redirectScript);
-    }
-
-    if (CurrentUrl.substring(0, 18) == 'https://m.facebook') {
-      setShowSpinner(true);
-      Linking.openURL(CurrentUrl);
-      const redirectScript = "window.location.href = 'https://appskvt.charvns.com/';";
-      webViewRef.current.injectJavaScript(redirectScript);
-    }
-
-    if (CurrentUrl === 'https://twitter.com/ShriVishwanath') {
-      setShowSpinner(true);
-      Linking.openURL('https://twitter.com/ShriVishwanath');
-      const redirectScript = "window.location.href = 'https://appskvt.charvns.com/';";
-      webViewRef.current.injectJavaScript(redirectScript);
-    }
-
-    if (CurrentUrl === 'https://api.whatsapp.com/send/?phone=918528996606&text&type=phone_number&app_absent=0') {
-      setShowSpinner(true);
-      Linking.openURL('https://api.whatsapp.com/send/?phone=918528996606&text&type=phone_number&app_absent=0');
-      const redirectScript = "window.location.href = 'https://appskvt.charvns.com/';";
-      webViewRef.current.injectJavaScript(redirectScript);
-    }
-
-  }, [CurrentUrl]);
-  const handleLoadStart = () => {
-    setShowSpinner(true);
-  };
-
-  const handleLoadEnd = () => {
-    setShowSpinner(false);
-  };
-
-  const handleError = () => {
-    setShowSpinner(false);
-    Alert.alert('Error', 'An error occurred while loading the page.');
-  };
-
-  const handleLoadProgress = ({ nativeEvent }) => {
-    if (nativeEvent.progress === 1) {
-      setShowSpinner(false);
-    }
-  };
-
-  const resetScrollPosition = () => {
-    webViewRef.current.injectJavaScript(`window.scrollTo(0, 0);true;`);
-  };
   useEffect(() => {
     const unsubscribe = NetInfo.addEventListener(state => {
       setIsConnected(state.isConnected);
@@ -93,16 +20,7 @@ const App = () => {
       if (canGoBack) {
         webViewRef.current.goBack();
         return true; // Prevent default behavior (exit app)
-      }
-      if (CurrentUrl === 'https://appskvt.charvns.com/BookingOrderList') {
-        const redirectScript = "window.location.href = 'https://appskvt.charvns.com/';";
-        webViewRef.current.injectJavaScript(redirectScript);
-      }
-      if (CurrentUrl === 'https://appskvt.charvns.com/DonationList') {
-        const redirectScript = "window.location.href = 'https://appskvt.charvns.com/';";
-        webViewRef.current.injectJavaScript(redirectScript);
-      }
-      if (CurrentUrl === 'https://appskvt.charvns.com/') {
+      } else {
         Alert.alert(
           'Exit App',
           'Do you want to exit the app?',
@@ -119,10 +37,8 @@ const App = () => {
           ],
           { cancelable: false }
         );
-      } else {
-        webViewRef.current.goBack();
+        return true; // Prevent default behavior (exit app)
       }
-      return true; // Prevent default behavior (exit app)
     };
 
     BackHandler.addEventListener('hardwareBackPress', onBackPress);
@@ -130,7 +46,7 @@ const App = () => {
       unsubscribe();
       BackHandler.removeEventListener('hardwareBackPress', onBackPress);
     };
-  }, [canGoBack, CurrentUrl]);
+  }, [canGoBack]);
 
   if (isConnected === null) {
     return (
@@ -149,15 +65,7 @@ const App = () => {
   }
 
   return (
-    <SafeAreaView style={styles.safeArea}>
-      {/* Translucent StatusBar */}
-      <StatusBar translucent backgroundColor="transparent" barStyle="light-content" />
-
-      {/* Gradient View for StatusBar */}
-      <LinearGradient
-        colors={['#FF5733', '#FFBD33']} // Define your gradient colors here
-        style={styles.gradient}
-      />
+    <View style={{ flex: 1 }}>
       <Spinner
         visible={showSpinner}
         textContent={'Please Wait ..'}
@@ -166,33 +74,19 @@ const App = () => {
         color="#bb5533"
       />
       <WebView
-        source={{ uri: 'https://appskvt.charvns.com/' }}
+        source={{ uri: 'https://divya.charvns.com/' }}
+        onLoadStart={() => setShowSpinner(true)}
+        onLoad={() => setShowSpinner(false)}
+        onError={() => setShowSpinner(false)}
         ref={webViewRef}
         onNavigationStateChange={navState => {
-          const { url } = navState;
-          setCurrentUrl(url);
-          if (url === 'https://secure.ccavenue.com/cancelTransaction') {
-            if (webViewRef.current) {
-              const redirectScript = "window.location.href = 'https://appskvt.charvns.com/';";
-              webViewRef.current.injectJavaScript(redirectScript);
-            }
-          }
-          if (!navState.loading) {
-            setShowSpinner(false);
-            resetScrollPosition();
+          setCanGoBack(navState.canGoBack);
+          if (navState.url === 'https://divya.charvns.com/') {
+            setCanGoBack(false);
           }
         }}
-        injectedJavaScript={injectedJavaScript}
-        onLoadStart={handleLoadStart}
-        onLoadEnd={() => {
-          handleLoadEnd();
-          resetScrollPosition();
-        }}
-        onError={handleError}
-        onHttpError={handleError} // Handles HTTP errors
-        onLoadProgress={handleLoadProgress}
       />
-    </SafeAreaView>
+    </View>
   );
 };
 
@@ -206,16 +100,5 @@ const styles = StyleSheet.create({
   },
   webview: {
     flex: 1,
-  },
-  safeArea: {
-    flex: 1,
-    backgroundColor: '#FFFFFF',
-  },
-  gradient: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    height: StatusBar.currentHeight, // Height of the status bar area
   },
 });
